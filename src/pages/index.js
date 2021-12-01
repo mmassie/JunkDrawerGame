0071ec9b3ec69dff77a857aeb9ea00ca80523cb2
Mike Massie <mike@michaelmassie.com>
2021-12-01 08:43:50 -0600

    transition

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ import App from "../components/app";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Clutter score your junk drawer</h1>
-    <p>Tally up you score and let's hear it:</p>
+    <h1>Let's play Bingo!</h1>
+    <p>Tally up you score when you hear them:</p>
     <App />
   </Layout>
 )
