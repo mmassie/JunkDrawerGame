167bf604e398bccf7b1f7528b120cf46face3b3f
Mike Massie <mike@michaelmassie.com>
2024-05-20 16:59:47 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ import App from "../components/app";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Clutter score your junk drawer</h1>
-    <p>Tally up you score and let's hear it:</p>
+    <h1>Elise's Cafe</h1>
+    <p>Today's Menu</p>
     <App />
   </Layout>
 )
